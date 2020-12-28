f460c17fbeeae1b3126daa774fabdd8f314f29d1
Whitefoxxx02 <asgarkhan16@gmail.com>
2020-12-29 04:55:08 +0530

    Implemeted Shopping Cart functionality will work on Customer tomorrow

---
+++ MyShop.UI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,46 @@
+﻿using MyShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.UI.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        IShoppingCartService ShoppingCartService;
+
+        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        {
+            this.ShoppingCartService = shoppingCartService;
+        }
+
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var model = ShoppingCartService.GetCartItems(HttpContext);
+            return View(model);
+        }
+
+        public ActionResult AddToCart(string Id)
+        {
+            ShoppingCartService.AddToCart(HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(string Id)
+        {
+            ShoppingCartService.RemoveFromCart(HttpContext, Id);
+            return RedirectToAction("Index");
+        }
+
+        //Partial view for shopping cart summary
+        public PartialViewResult ShoppingCartSummary()
+        {
+            var cartSummary = ShoppingCartService.GetCartSummary(HttpContext);
+            return PartialView(cartSummary);
+        }
+    }
+}
